c77b06ceda36a8ab2fd1b310cb2d0798530bce4b
wrosner <wrosner@kellerkind.rosner.lokal>
2024-06-07 12:36:53 +0200

    solver for FC Spreadsheet

---
+++ reverseKinematicSolver.py
@@ -0,0 +1,16 @@
+import FreeCAD
+
+import numpy as np
+from scipy.optimize import fsolve
+# https://realpython.com/python-pretty-print/
+import pprint
+
+
+def dummy(*args):
+    # print(args)
+    print("dummy for reverse kinematic solver")
+    pprint.pprint(args)
+
+
+# reverse kinematic solver
+
